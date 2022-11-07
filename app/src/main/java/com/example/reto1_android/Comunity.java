7ea561e608d11b1f3a09eb8267330fcf03e26fa7
Adr10 <64582247+Adr10@users.noreply.github.com>
2022-11-07 19:28:41 +0100

    Diseño de LOGIN y REGISTRO

---
+++ app/src/main/java/com/example/reto1_android/Comunity.java
@@ -0,0 +1,21 @@
+package com.example.reto1_android;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Comunity  extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_comunity);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
---
+++ app/src/main/java/com/example/reto1_android/Favoritos.java
@@ -0,0 +1,20 @@
+package com.example.reto1_android;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Favoritos extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favoritos);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}

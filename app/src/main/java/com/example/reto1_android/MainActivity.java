4daf9a06378a59a699757ecebdb8079e8a2464cd
Adr10 <64582247+Adr10@users.noreply.github.com>
2022-11-10 20:57:46 +0100

    Funcion Checkbox

--- app/src/main/java/com/example/reto1_android/MainActivity.java
+++ app/src/main/java/com/example/reto1_android/MainActivity.java
@@ -1,11 +1,23 @@
 package com.example.reto1_android;
 
-import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.beans_SQLite.Client;
+import com.example.datamanager_SQLite.DataManager;
+
+import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private Button botonRegistro = null;
@@ -15,23 +27,71 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        botonRegistro = (Button) findViewById( R.id.button3);
-        botonRegistro.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent ActivityOpcion = new Intent(MainActivity.this, Registro.class);
-                startActivity(ActivityOpcion);
-            }
+        /*-------------------SQLite---------------------*/
+        /*Funcion de CheckBox*/
+        DataManager dataManager = new DataManager(this);
 
-        });
+        ((EditText) findViewById( R.id.editTextTextPersonName )).addTextChangedListener( registerEditTextListener );
+        ((EditText) findViewById( R.id.editTextTextPassword )).addTextChangedListener( registerEditTextListener );
+        CheckBox checkBox = (CheckBox) findViewById (R.id.checkBox);
+
+        ((EditText) findViewById( R.id.editTextTextPersonName )).setText( dataManager.selectUltimo().getLoginUser() );
+        ((EditText) findViewById( R.id.editTextTextPassword )).setText( dataManager.selectUltimo().getPass());
 
 
         entrarComunity = (Button) findViewById( R.id.button2);
         entrarComunity.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent ActivityOpcion = new Intent(MainActivity.this, Comunity.class);
-                startActivity(ActivityOpcion);
+
+                String name = ((EditText) findViewById( R.id.editTextTextPersonName )).getText().toString();
+                String Pass = ((EditText) findViewById( R.id.editTextTextPassword )).getText().toString();
+
+
+                if (checkBox.isChecked()) {
+
+                    boolean found = false;
+                    if (!dataManager.isEmpty()) {
+                        List<Client> clients = dataManager.selectAllClients();
+
+                        for (Client client : clients) {
+                            found = (client.getLoginUser().equals(name)) && (client.getPass().equals(Pass));
+                            if (found)
+                                break;
+                        }
+                    }
+
+                    if (!found) {
+                        try {
+                            Client cliente = new Client();
+                            cliente.setLoginUser(name);
+                            cliente.setPass(Pass);
+
+                            dataManager.insert(cliente);
+
+                            Toast.makeText(getApplicationContext(),"Cliente Guardado", Toast.LENGTH_SHORT);
+
+                        } catch (Exception e) {
+                            Toast.makeText(getApplicationContext(),"Error de BBDD", Toast.LENGTH_SHORT);
+                        }
+                    } else {
+                        Toast.makeText(getApplicationContext(),"Cliente Repetido", Toast.LENGTH_SHORT);
+                    }
+                }
+
+                startActivity(new Intent(MainActivity.this, Comunity.class));
+
+
+
+            }
+
+        });
+
+        botonRegistro = (Button) findViewById( R.id.button3);
+        botonRegistro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Registro.class));
             }
 
         });
@@ -39,17 +99,44 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
 
-
-
-
-
-
-
-
-
     }
+
     @Override
     public void onClick(View v) {
 
+
+
+
     }
+
+
+    //------------------------- EditText Events -------------------------//
+
+    /**
+     * Disables the list button if editTexts are empty
+     */
+    private final TextWatcher registerEditTextListener = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            String name = ((EditText) findViewById( R.id.editTextTextPersonName )).getText().toString();
+            String Pass = ((EditText) findViewById( R.id.editTextTextPassword )).getText().toString();
+
+
+            (findViewById( R.id.button2 )).setEnabled( !TextUtils.isEmpty( name ) && !TextUtils.isEmpty( Pass ));
+        }
+    };
+
+
+
+
+
+
 }
--- app/src/main/java/com/example/reto1_android/Registro.java
+++ app/src/main/java/com/example/reto1_android/Registro.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +12,9 @@ import androidx.appcompat.app.AppCompatActivity;
 public class Registro extends AppCompatActivity implements View.OnClickListener{
     private Button Aceptar = null;
     private ImageButton Cancelar = null;
+    private EditText pass = null;
+    private EditText passDos = null;
+    private boolean respuesta = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,10 +24,17 @@ public class Registro extends AppCompatActivity implements View.OnClickListener{
 
         Aceptar = (Button) findViewById(R.id.button6);
         Aceptar.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
-                Intent ActivityOpcion = new Intent(Registro.this, MainActivity.class);
-                startActivity(ActivityOpcion);
+
+                comprovacionPass();
+                comprovacionUsuarioRegistrado();
+                if (respuesta== true){
+                    startActivity(new Intent(Registro.this, MainActivity.class));
+
+                }
+
             }
 
         });
@@ -33,21 +44,33 @@ public class Registro extends AppCompatActivity implements View.OnClickListener{
         Cancelar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent ActivityOpcion = new Intent(Registro.this, MainActivity.class);
-                startActivity(ActivityOpcion);
+                startActivity(new Intent(Registro.this, MainActivity.class));
             }
 
         });
 
 
 
+    }
+
+    public boolean comprovacionPass(){
+
+        pass = (EditText) findViewById(R.id.editTextTextPassword);
+        passDos = (EditText) findViewById(R.id.editTextTextPassword2);
+
+            if (pass.equals(passDos)){
+            respuesta= true;
+             }
+        return respuesta;
+    }
+
+    public boolean comprovacionUsuarioRegistrado(){
+
+        /* Necesario pedir USER de la BBD */
 
 
 
-
-
-
-
+        return respuesta;
     }
 
 
